b6c72c3d9565644870c5316a1a1451e0fbc199da
Linf0rd <linfordsnagger@gmx.com>
2025-04-16 18:33:16 +0200

    Update layout and home page content; add dashboard page.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <button className="btn">Get Started</button>
+      <h1>Collect customer feedback to build better products.</h1>
+      <div>
+        Create a feedback board in minutes, prioritize features, and
+        build products your customers will love.
+      </div>
+      <a href="/dashboard">Dashboard</a>
     </main>
   );
 }
